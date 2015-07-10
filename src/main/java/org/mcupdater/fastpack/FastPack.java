06f21dab287f18da197165b2d0d43d4760d30470
Scott M. Barbour <smbarbour@gmail.com>
2015-07-10 13:29:07 -0500

    Cleanup formatting

--- src/main/java/org/mcupdater/fastpack/FastPack.java
+++ src/main/java/org/mcupdater/fastpack/FastPack.java
@@ -18,11 +18,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
-public class FastPack
-{
-	public static final Map<String,String> modExceptions = new HashMap<>();
+public class FastPack {
+	public static final Map<String, String> modExceptions = new HashMap<>();
 	public static final Map<String, String> configExceptions = new HashMap<>();
-    public static boolean hasForge = false;
+	public static boolean hasForge = false;
 	public static boolean hasLitemods = false;
 	private static boolean debug = false;
 	private static boolean doConfigs = true;
@@ -30,39 +29,39 @@ public class FastPack
 
 	public static void main(final String[] args) {
 		OptionParser optParser = new OptionParser();
-        optParser.accepts("help","Shows this help").isForHelp();
-        optParser.formatHelpWith(new BuiltinHelpFormatter(200,3));
-		ArgumentAcceptingOptionSpec<String> searchPathSpec = optParser.accepts("path","Path to scan for mods and configs").requiredUnless("help").withRequiredArg().ofType(String.class);
-		ArgumentAcceptingOptionSpec<String> baseURLSpec = optParser.accepts("baseURL","Base URL for downloads").requiredUnless("help").withRequiredArg().ofType(String.class);
-		ArgumentAcceptingOptionSpec<String> MCVersionSpec = optParser.accepts("mc","Minecraft version").requiredUnless("help").withRequiredArg().ofType(String.class);
-		ArgumentAcceptingOptionSpec<String> forgeVersionSpec = optParser.accepts("forge","Forge version").withRequiredArg().ofType(String.class);
-		ArgumentAcceptingOptionSpec<String> xmlPathSpec = optParser.accepts("out","XML file to write").requiredUnless("help").withRequiredArg().ofType(String.class);
-		ArgumentAcceptingOptionSpec<String> serverAddrSpec = optParser.accepts("mcserver","Server address").withRequiredArg().ofType(String.class).defaultsTo("");
-		ArgumentAcceptingOptionSpec<String> serverNameSpec = optParser.accepts("name","Server name").withRequiredArg().ofType(String.class).defaultsTo("FastPack Instance");
-		ArgumentAcceptingOptionSpec<String> serverIdSpec = optParser.accepts("id","Server ID").withRequiredArg().ofType(String.class).defaultsTo("fastpack");
-        ArgumentAcceptingOptionSpec<String> mainClassSpec = optParser.accepts("mainClass","Main class for launching Minecraft").withRequiredArg().ofType(String.class).defaultsTo("net.minecraft.launchwrapper.Launch");
-        ArgumentAcceptingOptionSpec<String> newsURLSpec = optParser.accepts("newsURL","URL to display in the News tab").withRequiredArg().ofType(String.class).defaultsTo("about:blank");
-        ArgumentAcceptingOptionSpec<String> iconURLSpec = optParser.accepts("iconURL","URL of icon to display in instance list").withRequiredArg().ofType(String.class).defaultsTo("");
-        ArgumentAcceptingOptionSpec<String> revisionSpec = optParser.accepts("revision","Revision string to display").withRequiredArg().ofType(String.class).defaultsTo("1");
-        ArgumentAcceptingOptionSpec<Boolean> autoConnectSpec = optParser.accepts("autoConnect","Auto-connect to server on launch").withRequiredArg().ofType(Boolean.class).defaultsTo(Boolean.TRUE);
-        ArgumentAcceptingOptionSpec<String> stylesheetSpec = optParser.accepts("xslt","Path of XSLT file").withRequiredArg().ofType(String.class).defaultsTo("");
-		optParser.accepts("noConfigs","Do not generate ConfigFile entries");
-		optParser.accepts("configsOnly","Generate all mods as overrides with ConfigFile entries");
-		optParser.accepts("debug","Output full config matching data");
+		optParser.accepts("help", "Shows this help").isForHelp();
+		optParser.formatHelpWith(new BuiltinHelpFormatter(200, 3));
+		ArgumentAcceptingOptionSpec<String> searchPathSpec = optParser.accepts("path", "Path to scan for mods and configs").requiredUnless("help").withRequiredArg().ofType(String.class);
+		ArgumentAcceptingOptionSpec<String> baseURLSpec = optParser.accepts("baseURL", "Base URL for downloads").requiredUnless("help").withRequiredArg().ofType(String.class);
+		ArgumentAcceptingOptionSpec<String> MCVersionSpec = optParser.accepts("mc", "Minecraft version").requiredUnless("help").withRequiredArg().ofType(String.class);
+		ArgumentAcceptingOptionSpec<String> forgeVersionSpec = optParser.accepts("forge", "Forge version").withRequiredArg().ofType(String.class);
+		ArgumentAcceptingOptionSpec<String> xmlPathSpec = optParser.accepts("out", "XML file to write").requiredUnless("help").withRequiredArg().ofType(String.class);
+		ArgumentAcceptingOptionSpec<String> serverAddrSpec = optParser.accepts("mcserver", "Server address").withRequiredArg().ofType(String.class).defaultsTo("");
+		ArgumentAcceptingOptionSpec<String> serverNameSpec = optParser.accepts("name", "Server name").withRequiredArg().ofType(String.class).defaultsTo("FastPack Instance");
+		ArgumentAcceptingOptionSpec<String> serverIdSpec = optParser.accepts("id", "Server ID").withRequiredArg().ofType(String.class).defaultsTo("fastpack");
+		ArgumentAcceptingOptionSpec<String> mainClassSpec = optParser.accepts("mainClass", "Main class for launching Minecraft").withRequiredArg().ofType(String.class).defaultsTo("net.minecraft.launchwrapper.Launch");
+		ArgumentAcceptingOptionSpec<String> newsURLSpec = optParser.accepts("newsURL", "URL to display in the News tab").withRequiredArg().ofType(String.class).defaultsTo("about:blank");
+		ArgumentAcceptingOptionSpec<String> iconURLSpec = optParser.accepts("iconURL", "URL of icon to display in instance list").withRequiredArg().ofType(String.class).defaultsTo("");
+		ArgumentAcceptingOptionSpec<String> revisionSpec = optParser.accepts("revision", "Revision string to display").withRequiredArg().ofType(String.class).defaultsTo("1");
+		ArgumentAcceptingOptionSpec<Boolean> autoConnectSpec = optParser.accepts("autoConnect", "Auto-connect to server on launch").withRequiredArg().ofType(Boolean.class).defaultsTo(Boolean.TRUE);
+		ArgumentAcceptingOptionSpec<String> stylesheetSpec = optParser.accepts("xslt", "Path of XSLT file").withRequiredArg().ofType(String.class).defaultsTo("");
+		optParser.accepts("noConfigs", "Do not generate ConfigFile entries");
+		optParser.accepts("configsOnly", "Generate all mods as overrides with ConfigFile entries");
+		optParser.accepts("debug", "Output full config matching data");
 		final OptionSet options = optParser.parse(args);
 
-        if (options.has("help")) {
-            try {
-                optParser.printHelpOn(System.out);
-                return;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+		if (options.has("help")) {
+			try {
+				optParser.printHelpOn(System.out);
+				return;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 
-        if (options.has("forge")) {
-            hasForge = true;
-        }
+		if (options.has("forge")) {
+			hasForge = true;
+		}
 
 		if (options.has("debug")) {
 			debug = true;
@@ -85,15 +84,15 @@ public class FastPack
 		entry.setAddress(serverAddrSpec.value(options));
 		entry.setMainClass(mainClassSpec.value(options));
 		entry.setNewsUrl(newsURLSpec.value(options));
-        entry.setIconUrl(iconURLSpec.value(options));
-        entry.setRevision(revisionSpec.value(options));
-        entry.setAutoConnect(autoConnectSpec.value(options));
+		entry.setIconUrl(iconURLSpec.value(options));
+		entry.setRevision(revisionSpec.value(options));
+		entry.setAutoConnect(autoConnectSpec.value(options));
 		entry.setVersion(MCVersionSpec.value(options));
 		definition.setServerEntry(entry);
-        if (hasForge) {
-            definition.addImport(new Import("http://files.mcupdater.com/example/forge.php?mc=" + MCVersionSpec.value(options) + "&forge=" + forgeVersionSpec.value(options), "forge"));
-        }
-        String stylesheet = stylesheetSpec.value(options);
+		if (hasForge) {
+			definition.addImport(new Import("http://files.mcupdater.com/example/forge.php?mc=" + MCVersionSpec.value(options) + "&forge=" + forgeVersionSpec.value(options), "forge"));
+		}
+		String stylesheet = stylesheetSpec.value(options);
 
 		Path searchPath = new File(searchPathSpec.value(options)).toPath();
 		Path xmlPath = new File(xmlPathSpec.value(options)).toPath();
@@ -104,25 +103,25 @@ public class FastPack
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-        if (debug) {
-            for (Module modEntry: definition.getModules()) {
-                System.out.println(modEntry.toString());
-            }
-        }
+		if (debug) {
+			for (Module modEntry : definition.getModules()) {
+				System.out.println(modEntry.toString());
+			}
+		}
 		definition.sortMods();
-        if (hasForge) {
-            definition.addModule(new Module("Minecraft Forge", "forge-" + forgeVersionSpec.value(options), new ArrayList<PrioritizedURL>(), "", true, ModType.Override, 0, false, false, true, "", new ArrayList<ConfigFile>(), "BOTH", "", new HashMap<String, String>(), "", "", new ArrayList<GenericModule>(), ""));
-        }
-        if (doConfigs) definition.assignConfigs(debug);
+		if (hasForge) {
+			definition.addModule(new Module("Minecraft Forge", "forge-" + forgeVersionSpec.value(options), new ArrayList<PrioritizedURL>(), "", true, ModType.Override, 0, false, false, true, "", new ArrayList<ConfigFile>(), "BOTH", "", new HashMap<String, String>(), "", "", new ArrayList<GenericModule>(), ""));
+		}
+		if (doConfigs) definition.assignConfigs(debug);
 
 		try {
 			BufferedWriter fileWriter = Files.newBufferedWriter(xmlPath, StandardCharsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE);
 			fileWriter.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
 			fileWriter.newLine();
-            if (!stylesheet.isEmpty()) {
-                fileWriter.write("<?xml-stylesheet href=\"" + xmlEscape(stylesheet) + "\" type=\"text/xsl\" ?>");
-                fileWriter.newLine();
-            }
+			if (!stylesheet.isEmpty()) {
+				fileWriter.write("<?xml-stylesheet href=\"" + xmlEscape(stylesheet) + "\" type=\"text/xsl\" ?>");
+				fileWriter.newLine();
+			}
 			fileWriter.write("<ServerPack version=\"" + org.mcupdater.api.Version.API_VERSION + "\" xmlns=\"http://www.mcupdater.com\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.mcupdater.com http://files.mcupdater.com/ServerPackv2.xsd\">");
 			fileWriter.newLine();
 			fileWriter.write("\t<Server id=\"" + xmlEscape(definition.getServerEntry().getServerId()) +
@@ -131,9 +130,9 @@ public class FastPack
 					"\" version=\"" + xmlEscape(definition.getServerEntry().getVersion()) +
 					"\" mainClass=\"" + xmlEscape(definition.getServerEntry().getMainClass()) +
 					(definition.getServerEntry().getAddress().isEmpty() ? "" : ("\" serverAddress=\"" + xmlEscape(definition.getServerEntry().getAddress()))) +
-                    (definition.getServerEntry().getIconUrl().isEmpty() ? "" : ("\" iconUrl=\"" + xmlEscape(definition.getServerEntry().getIconUrl()))) +
-                    "\" revision=\"" + xmlEscape(definition.getServerEntry().getRevision()) +
-                    "\" autoConnect=\"" + Boolean.toString(definition.getServerEntry().isAutoConnect()) +
+					(definition.getServerEntry().getIconUrl().isEmpty() ? "" : ("\" iconUrl=\"" + xmlEscape(definition.getServerEntry().getIconUrl()))) +
+					"\" revision=\"" + xmlEscape(definition.getServerEntry().getRevision()) +
+					"\" autoConnect=\"" + Boolean.toString(definition.getServerEntry().isAutoConnect()) +
 					"\">");
 			fileWriter.newLine();
 			for (Import importEntry : definition.getImports()) {
@@ -263,8 +262,8 @@ public class FastPack
 					fileWriter.newLine();
 					fileWriter.write("\t\t\t\t<Path>" + xmlEscape(config.getPath()) + "</Path>");
 					fileWriter.newLine();
-                    fileWriter.write("\t\t\t\t<NoOverwrite>" + config.isNoOverwrite() + "</NoOverwrite>");
-                    fileWriter.newLine();
+					fileWriter.write("\t\t\t\t<NoOverwrite>" + config.isNoOverwrite() + "</NoOverwrite>");
+					fileWriter.newLine();
 					fileWriter.write("\t\t\t\t<MD5>" + xmlEscape(config.getMD5()) + "</MD5>");
 					fileWriter.newLine();
 					fileWriter.write("\t\t\t</ConfigFile>");
@@ -284,29 +283,29 @@ public class FastPack
 	}
 
 	private static void initExceptions() {
-		modExceptions.put("NotEnoughItems","NEI");
-		modExceptions.put("AWWayofTime","BloodMagic");
-		modExceptions.put("WR-CBE|Core","WirelessRedstone");
-		modExceptions.put("TConstruct","TinkersWorkshop");
-		modExceptions.put("inventorytweaks","InvTweaks");
-		modExceptions.put("ProjRed|Core","ProjectRed");
-		configExceptions.put("AWWayofTime","BloodMagic");
-		configExceptions.put("microblocks","ForgeMultipart");
-		configExceptions.put("cofh/world","CoFHCore");
-		configExceptions.put("cofh/Lexicon-Whitelist","CoFHCore");
-		configExceptions.put("hqm","HardcoreQuesting");
-		configExceptions.put("forgeChunkLoading","forge-\\d+.\\d+.\\d+.\\d+");
-        configExceptions.put("scripts","MineTweaker3");
-		configExceptions.put(".zs","MineTweaker3");
+		modExceptions.put("NotEnoughItems", "NEI");
+		modExceptions.put("AWWayofTime", "BloodMagic");
+		modExceptions.put("WR-CBE|Core", "WirelessRedstone");
+		modExceptions.put("TConstruct", "TinkersWorkshop");
+		modExceptions.put("inventorytweaks", "InvTweaks");
+		modExceptions.put("ProjRed|Core", "ProjectRed");
+		configExceptions.put("AWWayofTime", "BloodMagic");
+		configExceptions.put("microblocks", "ForgeMultipart");
+		configExceptions.put("cofh/world", "CoFHCore");
+		configExceptions.put("cofh/Lexicon-Whitelist", "CoFHCore");
+		configExceptions.put("hqm", "HardcoreQuesting");
+		configExceptions.put("forgeChunkLoading", "forge-\\d+.\\d+.\\d+.\\d+");
+		configExceptions.put("scripts", "MineTweaker3");
+		configExceptions.put(".zs", "MineTweaker3");
 	}
 
 	private static String xmlEscape(String input) {
-        String result;
+		String result;
 		try {
-            result = input.replace("&","&amp;").replace("\"", "&quot;").replace("'", "&apos;").replace("<", "&lt;").replace(">","&gt;");
-        } catch (Exception e) {
-            result = "!!!! Error !!!!";
-        }
-        return result;
+			result = input.replace("&", "&amp;").replace("\"", "&quot;").replace("'", "&apos;").replace("<", "&lt;").replace(">", "&gt;");
+		} catch (Exception e) {
+			result = "!!!! Error !!!!";
+		}
+		return result;
 	}
 }
